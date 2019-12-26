1d2a54080f54a2e653c63df2ebb9b55b74b8be1c
Gubar Sergey <sergey.gubar187@gmail.com>
2019-12-26 14:25:07 +0200

    Add lab 4

---
+++ lab4/app/src/main/java/io/github/gubarsergey/lab4/CompassActivity.java
@@ -0,0 +1,59 @@
+package io.github.gubarsergey.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ImageView image;
+
+    private float currentDegree = 0f;
+
+    TextView topText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        image = findViewById(R.id.imageViewCompass);
+        topText = findViewById(R.id.tvHeading);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if (sensorManager != null) {
+            sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                    SensorManager.SENSOR_DELAY_FASTEST);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float degree = Math.round(event.values[0]);
+
+        topText.setText("Heading: " + degree + " degrees");
+
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        image.startAnimation(ra);
+        currentDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
---
+++ lab4/app/src/main/java/io/github/gubarsergey/lab4/MainActivity.java
@@ -0,0 +1,63 @@
+package io.github.gubarsergey.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    private Sensor lightSensor;
+    private SensorManager lightSensorManager;
+    private TextView lightTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lightTextView = findViewById(R.id.light_text_view);
+
+
+        lightSensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+
+        lightSensor = lightSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        if (lightSensor == null) {
+            lightTextView.setText("No sensor");
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (lightSensor != null) {
+            lightSensorManager.registerListener(this, lightSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        lightSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float currentValue = sensorEvent.values[0];
+        lightTextView.setText("Light value (lux): " + currentValue);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
